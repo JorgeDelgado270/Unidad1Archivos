0902661d800fb644bbc149f6dd615d84e04e6694
JorgeDelgado270 <joardego6@gmail.com>
2024-11-22 19:06:17 -0600

    Add files via upload

---
+++ UNIDAD 3/PRACTICAS INDIVIDUALES/PRACTICA 2/PRACTICA 2/Form1.cs
@@ -0,0 +1,20 @@
+namespace PRACTICA_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Hello. " + textBox1.Text + "!";
+        }
+    }
+}
